ba5ddd3b511e75bd7e0f131a2ab7a8e1fdbacb1d
mohsinarifdev <mohsin.arif@digitalelites.dev>
2025-01-09 01:46:50 +0500

    Update recap_v2_callback.py

--- v2/recap_v2_callback.py
+++ v2/recap_v2_callback.py
@@ -8,7 +8,7 @@ from twocaptcha import TwoCaptcha
 from selenium.webdriver.chrome.options import Options
 import time
 
-API_KEY="376856f5386c731f51e98830324cfda5"
+API_KEY="your api key"
 print("key is loaded !")
 
 solver = TwoCaptcha(API_KEY)
